beb550e2c0eea52aa61c9504d92c6038953d0e50
bdsqqq <igorbedesqui@gmail.com>
2020-10-19 23:57:41 -0300

    added basic graphql implementation

---
+++ pages/api/graphql.js
@@ -0,0 +1,54 @@
+import { ApolloServer, gql } from "apollo-server-micro";
+import { makeExecutableSchema } from "graphql-tools";
+import { connectToDatabase } from "../../util/mongodb";
+
+const typeDefs = gql`
+  type Project {
+    id: ID!
+    description: String
+    img: String
+    height: String
+    demo: String
+    name: String!
+    order: String!
+    role: String
+    source: String
+    tools: [String]
+    year: String
+  }
+
+  type Query {
+    projects: [Project]!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    projects(_parent, _args, _context, _info) {
+      return _context.db
+        .collection("projects")
+        .findOne()
+        .then((data) => {
+          return data.projects;
+        });
+    },
+  },
+};
+
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers,
+});
+
+const apolloServer = new ApolloServer({
+  schema,
+  context: async () => await connectToDatabase(),
+});
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+export default apolloServer.createHandler({ path: "/api/graphql" });
